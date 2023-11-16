20f4ff3bbc71dd5f0742e82a669f6fbe1ec7c980
Daniel Andres Abogado Guzman <daniel.abogado@avaldigitallabs.com>
2023-11-15 20:23:18 -0500

    cambios de version 4

--- src/main/java/io/github/dabogadog/models/BateryTemp/Data.java
+++ src/main/java/io/github/dabogadog/models/BateryTemp/Data.java
@@ -1,5 +1,7 @@
 package io.github.dabogadog.models.BateryTemp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Data {
     public Integer percent;
     public Double temp;
