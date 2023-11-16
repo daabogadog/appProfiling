60a4a697afb1d0d19862f30eda05ad6ea02123a5
Daniel Andres Abogado Guzman <daniel.abogado@avaldigitallabs.com>
2023-11-15 20:42:46 -0500

    cambios de version 6

--- src/main/java/io/github/dabogadog/models/Rendering/Data.java
+++ src/main/java/io/github/dabogadog/models/Rendering/Data.java
@@ -1,5 +1,8 @@
 package io.github.dabogadog.models.Rendering;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Data {
     public Float count;
 }
